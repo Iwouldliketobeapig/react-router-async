054a7ab1f9d3ae2615483d0cf009197892c62344
dutao <1173127007@qq.com>
2019-03-01 15:51:12 +0800

    fix: 带审批按钮解耦

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component, lazy, Suspense } from 'react';
-import './App.css';
-import { Router, Route, Switch } from 'react-router-dom';
+import './App.scss';
+import { Router, Route, Switch, NavLink } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 import asyncComponent from './asyncComponent';
 import Program from './Program';
+// window.performance.getEntriesByType("resource");
 
 const Program1 = lazy(() => import('./Program1'));
 const history = createBrowserHistory();
@@ -11,12 +12,17 @@ class App extends Component {
   render() {
     return (
       <Router history={history}>
-        <div className="App">
+        <div className="app">
+          <header>
+            <NavLink to="/">首页</NavLink>
+            <NavLink to="/program1">program1</NavLink>
+            <NavLink to="/program2">program2</NavLink>
+          </header>
           <Suspense fallback={<div>Loading...</div>}>
             <Switch>
-              <Route path='/' exact={true} component={Program} />
-              <Route path='/program1' component={Program1} />
-              <Route path='/program2' component={asyncComponent(() => import('./Program2'))} />
+              <Route path="/" exact={true} component={Program} />
+              <Route path="/program1" component={Program1} />
+              <Route path="/program2" component={asyncComponent(() => import("./Program2"))} />
               <Route component={Program} />
             </Switch>
           </Suspense>
